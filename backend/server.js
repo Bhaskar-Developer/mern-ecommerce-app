781dab95d0b0ec117bef70587553d5f802701ea4
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-02 13:44:33 +0530

    Add custom error handler and not found handler middlewares

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,6 +17,10 @@ app.get('/', (req, res) => {
 
 // Mount Routes
 app.use('/api/products', productRoutes)
+
+// Not Found and Error Middleware
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
