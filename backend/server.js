8ee719d09161f4fd58364ae1216991dc904640ae
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-04 18:26:26 +0530

    Add user login route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
 dotenv.config()
@@ -11,12 +12,15 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send("API is running...")
 })
 
 // Mount Routes
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // Not Found and Error Middleware
 app.use(notFound)
