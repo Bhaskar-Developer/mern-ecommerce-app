f83724d59a56419ddb0844703b7cec79ff3d18f6
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-06 21:59:03 +0530

    Add admin route to fetch users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controller/userController.js'
-import { protect } from '../middlewares/authMiddleware.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controller/userController.js'
+import { protect, admin } from '../middlewares/authMiddleware.js'
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
