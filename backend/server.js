3c2529cffd3a94647005cda167758e239111d815
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-07 16:29:44 +0530

    Add image file upload route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
 
@@ -23,9 +25,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 // route to get paypal clientId
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+// make uploads folder static so that files can be uploaded to it
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // Not Found and Error Middleware
 app.use(notFound)
