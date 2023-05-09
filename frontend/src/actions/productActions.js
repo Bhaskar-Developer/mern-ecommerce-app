bb3f95a676529edd29c778243bf4e81c63e0a7ae
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-09 16:24:22 +0530

    Add product caurosel

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL } from '../constants/productContants'
+import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL, PRODUCT_TOP_REQUEST, PRODUCT_TOP_SUCCESS, PRODUCT_TOP_FAIL } from '../constants/productContants'
 
 export const listProducts = (keyword='', pageNumber= '') => async (dispatch) => {
   try {
@@ -154,6 +154,26 @@ export const createProductReview = (id, review) => async (dispatch, getState) =>
       // if there is an error
   dispatch({
       type: PRODUCT_CREATE_REVIEW_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message
+    })
+  }
+}
+
+export const listTopProducts = () => async (dispatch) => {
+  try {
+    // Loading is true
+    dispatch({ type: PRODUCT_TOP_REQUEST })
+
+    // Fetch Data
+    const { data } = await axios.get(`/api/products/top`)
+    dispatch({
+      type: PRODUCT_TOP_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    // if there is an error
+    dispatch({
+      type: PRODUCT_TOP_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message
     })
   }
