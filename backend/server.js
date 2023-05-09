32bf25bb01be550feecf5e804710968fdc54a42a
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-09 17:55:26 +0530

    Prepare for production

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-    res.send("API is running...")
-})
-
 // Mount Routes
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -34,10 +30,21 @@ app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
 // route to get paypal clientId
+const __dirname = path.resolve()
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
+if(process.env.NODE_ENV === 'production') {
+    // make the build folder static
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    // if we go to / route in API in production mode then serve the index.html page from frontends build folder
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')) )
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running...")
+    })
+}
+
 // make uploads folder static so that files can be uploaded to it
-const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 // Not Found and Error Middleware
