4ac92c200e4abf191492cf9bc54e2e96f54eb74d
Bhaskar-Developer <koderulzstack@gmail.com>
2023-05-09 17:07:22 +0530

    Show custom metadata using react helmet

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -4,6 +4,7 @@ import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from "../components/Rating"
 import Loader from "../components/Loader"
 import Message from "../components/Message"
+import Meta from "../components/Meta"
 import { useState, useEffect } from "react"
 import { listProductDetails, createProductReview } from "../actions/productActions"
 import { PRODUCT_CREATE_REVIEW_RESET } from "../constants/productContants"
@@ -59,6 +60,7 @@ const ProductScreen = () => {
                 {error}
             </Message> : (
                 <>
+                <Meta title={product.name} />
                     <Row>
                         <Col md={6} >
                             <Image src={product.image} alt={product.name} fluid />
